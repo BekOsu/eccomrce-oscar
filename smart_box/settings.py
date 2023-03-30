2aa755cc7e15c9e14a4b1696297cc92e34e18919
abu <abubakersuliman@outlook.com>
2023-03-30 15:40:05 +0400

    hosting

--- smart_box/settings.py
+++ smart_box/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g-b(&h0igxwsoa$dwe8&%j%z108b^zqnsi)swbk=1^*-1j^lh3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 # Application definition
 
